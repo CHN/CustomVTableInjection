696e55a7ceb1893803959d9bd186cf00fe1f8b7b
Cihan Sakarya <cihansakaryacs@gmail.com>
2022-03-01 03:04:28 +0300

    Create CustomVTableInjection.cpp

---
+++ CustomVTableInjection.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct A
+{
+    int x = 1;
+    virtual void abc(int b){cout << x * b;}  
+};
+
+struct B : public A
+{
+    int y = 2;
+    virtual void abc(int b){cout << y * b;}
+};
+
+int main()
+{
+    void* t = malloc(16);
+    *(((int*)t)+3) = 6;
+    
+    *(void**)t = (void*)new void*();
+    void** v = (void**)*(void**)(t);
+    *v = (void*)&B::abc;
+    
+    A* a = (A*)t;
+    
+    a->abc(2);
+    
+    free(t);
+    
+    return 0;
+}
